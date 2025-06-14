7ec91d42c33b7f28956c3451ef269db166ad0ac4
LARM-TECHs <luisalbertoramirezmorales2@gmail.com>
2025-06-14 19:25:27 -0400

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom"
 import Chat from './pages/ChatLLM/ChatPage.jsx'
 // import ChatNew from './pages/Chat.jsx'
 import LoginNew from './pages/ChatLLM/Login.jsx'
+import Administrador from './pages/GestionUsuario/Administrador.jsx'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       {/* <Route path="/" element={<Login />} /> */}
       {/* <Route path="/chat" element={<ChatNew />} /> */}
+      <Route path="/gestionUsuario" element={< Administrador/>} />
       <Route path="/chat" element={<Chat />} />
       <Route path="/" element={<LoginNew />} />
     </Routes>
