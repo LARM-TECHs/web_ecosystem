0deb43f8119c4a2c5386626225fd7ae2a762240f
ch4rl135 <carlozmarrero@gmail.com>
2025-06-22 20:41:24 -0400

    update backend/models/student.js

---
+++ backend/models/student.js
@@ -0,0 +1,32 @@
+// models/Student.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+class Student extends sequelize.Model {}
+
+Student.init({
+  student_id: {
+    type: DataTypes.STRING(50),
+    unique: true,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING(100),
+    unique: true,
+    allowNull: false,
+    validate: { isEmail: true }
+  }
+}, {
+  sequelize,
+  modelName: 'Student',
+  tableName: 'students',
+  timestamps: true,
+  createdAt: 'created_at',
+  updatedAt: false
+});
+
+module.exports = Student;
